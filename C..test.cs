01e980ec2d8c86471e961dbe58aae5aa2b22be7c
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2022-05-07 13:16:09 +0200

    t
    s

--- C..test.cs
+++ C..test.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Task
 
 namespace test
 {
@@ -10,7 +9,7 @@ namespace test
     {
        static void Main(string[] args)
        {
-           Console.WriteLine('Hello world');
+            Console.WriteLine("Hello world");
        }
     }
 }
