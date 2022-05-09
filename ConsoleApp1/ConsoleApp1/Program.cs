fbe43803c962eb694fe4a652fb1c264ad348f393
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2022-05-09 23:16:05 +0200

    update next step vi kan do
    https://www.youtube.com/watch?v=GhQdlIFylQ8&t=1162s
    
    30.00

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,12 +11,25 @@ namespace test
         {
             Console.WriteLine("Hello world");
             Console.WriteLine("hejdå");
-           
+
             Console.WriteLine("|__/");
             Console.WriteLine("|_/");
             Console.WriteLine("|/");
-            
+
+            string name = "Jhon";
+            int age; //två olika sätt att spara i variabel.
+            age = 20;
+
+            Console.WriteLine("here is once man named " + name);
+            Console.WriteLine("he was " + age + " years old");
+            Console.WriteLine("he really liked name " + name);
+            Console.WriteLine("But didnt like being " + age);
             Console.ReadLine();
         }
     }
-}
+}  
+
+
+        
+          
+             
