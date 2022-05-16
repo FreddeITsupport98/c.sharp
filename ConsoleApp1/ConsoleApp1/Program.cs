0b596607ff3a80b01029f50c8e803f32f53a9c90
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2022-05-16 23:34:34 +0200

    working with numbers
    data type is don now is working with number.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -25,13 +25,20 @@ namespace test
             Console.WriteLine("he really liked name " + name);
             Console.WriteLine("But didnt like being " + age);
 
-            string text = "giraff";
+            string text = "giraff" + "det är cool"; //konkatanering värden tillsammans
             char grade = 'A'; // Endast en karaktär ryms 
             int age2 = 30;
             double poäng = 3.5;
             bool IsMale = true; //ENDAST TRUE OR FALSE STATment
-            Console.WriteLine(text); 
-
+            Console.WriteLine(text);
+            Console.Write(text.Length); // .Length visar hur måånga ord det finns i strängen.
+            Console.WriteLine(text.ToUpper());
+            Console.WriteLine(text.Contains("giraff")); //.Contains slutar med true or false statment
+            Console.WriteLine(text.Contains("lol")); //.Contains report as false statment
+            Console.WriteLine(text[1]); //  0-9 first letter goes trough binary as it print second letter of first index output is = (I).
+            Console.WriteLine(text.IndexOf("giraff")); // tells where index of letter begin at.
+            Console.WriteLine(text.IndexOf("det är cool")); // same but other position of index
+            Console.WriteLine(text.Substring(2,4)); // lägger till substring som tar texten delvist som statartar fiörsta sen sista 
 
             Console.ReadLine();
 
