6e20a3320ed7db0a272dc2a3939f64e6cfeb9b2f
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2022-05-23 23:17:27 +0200

    yu
    gyi

---
+++ program testing/program testing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace test
+{
+    class program2
+    {
+        static void Main(string[] args)
+        {
+
+            bool male = true;
+            bool male2 = false
+            bool.Parse(male);
+            Console.WriteLine(male);
+
+            Console.ReadLine();
+        }
+    }
+}
