cc78ba17a92dfe6b0f5eb3efbe171186f5d34c48
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2022-05-24 22:49:14 +0200

    game dev
    dev

--- program testing/program testing/Program.cs
+++ program testing/program testing/Program.cs
@@ -9,13 +9,46 @@ namespace test
     {
         static void Main(string[] args)
         {
-            string userInput;
-            Console.WriteLine("Are you a male?: ");
-            userInput= Console.ReadLine();
-            bool userInputBoolean = Convert.ToBoolean(userInput);
-            Console.WriteLine(" Your answr " + userInputBoolean);
-            bool isValid = true;
-            bool isInvalid = false;
+            //string userInput;
+            //Console.WriteLine("Are you a male?: ");
+            //userInput= Console.ReadLine();
+            //bool userInputBoolean = Convert.ToBoolean(userInput);
+            //Console.WriteLine(" Your answr " + userInputBoolean);
+            //bool isValid = true;
+            //bool isInvalid = false;
+
+           int health = 100;
+           int damage = 10;
+
+           Console.WriteLine("Are you ready to defeat monster?");
+           Console.Write("Yes or No : ");
+           string answerone = Console.ReadLine();
+
+            Console.WriteLine(answerone);
+            if (answerone == "yes")
+            {
+                Console.WriteLine(health + " maximum HP");
+                Console.Write("attack?" + " yes or no ");
+                string answertwo = Console.ReadLine();
+                    if (answertwo == "yes")
+                {
+                    Console.WriteLine(health - damage);
+                    Console.WriteLine("You took som damge" + health);
+                } 
+                else if (answertwo == "no")
+                {
+
+                }
+            }
+            else if (answerone == "no")
+            {
+                Console.WriteLine("Goodbye no worries to defeat monster sleep tight...");
+            }
+            else 
+            {
+                 
+            }
+             
         }
     }
 }
