75aebe75fc33746e0dbd67b59e477d70b5b409d7
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2022-06-05 23:56:54 +0200

    tuples
    tuples

--- pythonprojekt/pythonprojekt/pythonprojekt.py
+++ pythonprojekt/pythonprojekt/pythonprojekt.py
@@ -1,4 +1,5 @@
 from math import *
+from typing import FrozenSet
 
 print("hello world")
 print(">")
@@ -42,8 +43,46 @@ print(sqrt(99))
 
 # Getting Input From Users
 
-name = input("enter your name: ")
-age1 = input("Your age: ")
-print(name + age)
+name2 = input("enter your name: ")
+age1 = int(input("Your age: "))
+print(name2, age1)
 
 # Building a Basic Calculator
+
+num1 = input("Enter a number: ")
+num2 = input("Enter a second number: ")
+result = float(num1) + float(num2) #float is decimal, int is whole number
+
+print(" Your number is: ", result)
+
+# Mad libs game
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a naun: ")
+celebrity = input("enter a celebrity: ")
+
+print("Roses are " + color)
+print("Violets are " + plural_noun)
+print("I love " + celebrity)
+
+# lists
+
+friends = ["Jesper", "Simon", "kasper", "karen", "fredrik", "karin"]
+friends[1] = "Super man!"
+print(friends[0:2])
+
+# lists funktion
+
+lucky_Number = [1, 2, 4, 8, 16, 32, 64, 128]
+friends_Second = ["Jesper", "Simon", "kasper", "karen", "fredrik", "karin"]
+#friends_Second.extend(lucky_Number) # ökar lista med annan lista
+#friends_Second.append("Creed") #bifogar lista
+#friends_Second.insert(0, "Kelly") #begin insert index starting number
+#friends_Second.remove("kasper")
+#friends_Second.pop()
+#friends_Second.sort() sorting out
+#friends_Second.reverse() reversing list
+# friends2 = friends_Second.copy copying list with same value as friends_second
+print(friends_Second)
+
+# Tuples
