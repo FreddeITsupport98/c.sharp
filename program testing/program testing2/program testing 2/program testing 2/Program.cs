823a89fde4761ccb1b399216020f3f7d450e545d
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2022-06-10 23:22:11 +0200

    I made ureka
    !!!!

---
+++ program testing/program testing2/program testing 2/program testing 2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace program_testing2
+{
+    class Program_testing2
+    {
+        static void Main(string[] args)
+        {
+           
+            string[] cars = new string[3];
+
+            cars[0] = "";
+            cars[1] = "";
+            cars[2] = "lol";
+
+            cars.Append(cars[1] = "Hejdå");
+            cars.Append(cars[2] = "");
+            
+
+            bool exit = false;
+            
+            while  (exit != true)
+            {
+                for (int i = 0; i < cars.Length; i++)
+                {
+                    Console.WriteLine(cars[i]);
+                }
+                Console.Write("menu 1 eller menu 2 eller menu3: ");
+                string menu = Console.ReadLine();
+                if (menu == "1")
+                {
+                    Console.WriteLine(cars);
+                }
+                else if (menu == "2")
+                {
+                    Console.WriteLine("menu2");
+                }
+                else if (menu == "3")
+                {
+                    Console.WriteLine("menu3");
+                    exit = true;
+                }
+                else
+                {
+                    Console.WriteLine("wrong keyboard");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
